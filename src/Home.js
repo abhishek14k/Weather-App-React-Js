143874b37f2de9243718c4ef382b773358f6f78f
Abhishek R <37078073+abhishek14k@users.noreply.github.com>
2023-04-24 09:26:24 +0530

    Added Weather Card

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,38 @@
 import React from "react";
+import "./style.css";
 
 function Home() {
   return (
-    <div>
-      <h1>Weather App</h1>
+    <div className="container">
+      <div className="weather">
+        <div className="search">
+          <input type="text" placeholder="Enter the city name" />
+          <button>
+            <img src="/images/search-image.png" alt="" />
+          </button>
+        </div>
+        <div className="winfo">
+          <img src="/images/clouds.png" alt="" />
+          <h1>22°c</h1>
+          <h2>London</h2>
+          <div className="details">
+            <div className="col">
+              <img src="/images/humidity.png" alt="" />
+              <div className="humidity">
+                <p>20%</p>
+                <p>Humidity</p>
+              </div>
+            </div>
+            <div className="col">
+              <img src="/images/wind.png" alt="" />
+              <div className="wind">
+                <p>2 km/hr</p>
+                <p>Wind</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
